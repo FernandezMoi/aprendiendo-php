9f86f875eb08656242fc80c0273f39baf205337b
Moifdez <moibetico@hotmail.es>
2022-12-09 14:45:00 +0100

    updated

---
+++ 09-bucle/index.php
@@ -0,0 +1,26 @@
+<?php
+
+// BUCLE WHILE
+/*
+Estructura de control que itera o repite la ejecución de una serie de 
+instrucciones las veces que sea necesario en base a la condición del bucle
+
+    while(condicion){
+        bloque de instrucciones
+        otras instrucciones
+    }
+*/
+
+// EJEMPLO 1
+$num = 0;
+while ($num <= 100) {
+    echo  "$num, ";
+    $num++;
+}
+//Ejemplo 2
+
+$num = 0;
+while ($num <= 1000) {
+    echo  "$num, ";
+    $num++;
+}
