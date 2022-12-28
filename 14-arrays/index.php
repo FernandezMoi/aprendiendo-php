24f388387856a4c4c1787de71f707a848562dd5c
FernandezMoi <moibetico@hotmail.e>
2022-12-28 14:06:05 +0100

    updated 28/12

--- 14-arrays/index.php
+++ 14-arrays/index.php
@@ -20,4 +20,52 @@ echo "<ul>";
 for ($i = 0; $i < count($peliculas); $i++) {
     echo "<li>$peliculas[$i]</li>";
 }
-echo "</ul>";
+echo "</ul><hr>";
+
+echo "<h1>Listado de cantantes</h1>";
+echo "<ul>";
+foreach ($cantantes as $cantante) {
+    echo "<li>$cantante</li>";
+}
+echo "</ul><hr>";
+
+//Arrays asociativo
+
+$personas = array(
+    'nombre' => 'Moi',
+    'apellidos' => 'Fdez',
+    'edad' => 25
+);
+
+var_dump($personas);
+echo "<br/>";
+echo $personas['apellidos'];
+
+echo "<br/><hr>";
+//Array multidimensional
+
+$contactos = array(
+    array(
+        'nombre' => 'Pepe',
+        'email' => 'pepe@pepe.com'
+    ),
+    array(
+        'nombre' => 'Jose',
+        'email' => 'jose@jose.com'
+    ),
+    array(
+        'nombre' => 'Kaiser',
+        'email' => 'kaiser@kaiser.com'
+    ),
+);
+
+//var_dump($contactos);
+
+echo $contactos[1]['nombre'];
+echo "<br/>";
+echo $contactos[2]['email'];
+echo "<br/>";
+
+foreach ($contactos as $key => $contacto) {
+    echo $contacto['nombre'] . "<br/>";
+}
